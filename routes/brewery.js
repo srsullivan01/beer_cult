ef1cd53c33285fbab19669d31872f50d6538c117
ctt1988 <clarence.tooles@gmail.com>
2017-07-28 12:11:16 -0400

    added brewery show routes and started on the new route

--- routes/brewery.js
+++ routes/brewery.js
@@ -1,9 +1,45 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('brewery/index', { title: 'Express' });
+var Brewery = require('../models/brewery');
+
+//index
+router.get('/', (request, response) => {
+    Brewery.find({})
+        .then((brewery) => {
+            response.render(
+                'brewery/index',
+                { brewery }
+            );
+        })
+        .catch((error) => {
+            console.log('Error retrieving brewerys from database!');
+            console.log(error);
+        });
+})
+
+
+//this is the create new form 
+router.get('/new', (request, response) => {
+	response.render('brewery/new');
+});
+
+
+// Brewery show route
+router.get('/:id', function(request, response, next) {
+
+    var breweryToSearchFor = request.params.id;
+
+    Brewery.findById(breweryToSearchFor)
+        .then((brewery) => {
+            response.render(
+                'brewery/show',
+                { brewery }
+            );
+        })
+        .catch((error) => {
+            console.log(`Error retrieving brewery with ID of ${breweryToSearchFor}`)
+        });
 });
 
 module.exports = router;
