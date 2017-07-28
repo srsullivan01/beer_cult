3e37384461d5552f63a474c163b2870a89572e3d
sbarnwell88 <susy6688@gmail.com>
2017-07-28 10:02:19 -0400

    added views folders and changed route names to match project

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var brewery = require('./routes/brewery');
+// var users = require('./routes/users');
+// var beers = require('./routes/beers');
 
 
 var app = express();
@@ -28,8 +29,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', brewery);
+// app.use('/users', users);
+// app.use('/beers', beers);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
